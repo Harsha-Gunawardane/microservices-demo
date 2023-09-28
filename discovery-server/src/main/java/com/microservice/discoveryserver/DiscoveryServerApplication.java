ba0a75146fec9d187d96c7097f93e1d8871abf74
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-09-28 17:02:33 +0530

    Issue occur with the netflix eureka dependency

--- discovery-server/src/main/java/com/microservice/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/microservice/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package com.microservice.orderservice;
+package com.microservice.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class OrderServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
-	}
-
+public class DiscoveryServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
 }
--- inventory-service/src/main/java/com/microservice/inventryservice/InventryServiceApplication.java
+++ inventory-service/src/main/java/com/microservice/inventryservice/InventryServiceApplication.java
@@ -5,9 +5,11 @@ import com.microservice.inventryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class InventryServiceApplication {
 
 	public static void main(String[] args) {
